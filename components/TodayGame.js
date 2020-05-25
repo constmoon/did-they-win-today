c5b41693d01c4142914e4c45f14450718faaec96
constmoon <jiwoo.link@gmail.com>
2020-05-26 05:17:44 +0900

    refactor: 경기 전과 경기 종료 구분

--- components/TodayGame.js
+++ components/TodayGame.js
@@ -1,36 +1,51 @@
 import React from 'react';
+import style from 'styles/todayGame.styl';
+
+const ScoreBoard = ({ date, away, home }) => {
+  if (!away.score && !home.score) {
+    away.score = 0;
+    home.score = 0;
+  }
+
+  return (
+    <div className={style.score}>
+      <div>{date}</div>
+      <span>{away.team}: {away.score}</span>
+      <span> vs</span>
+      <span> {home.team}: {home.score}</span>
+    </div>
+  )
+}
+const LiveStreaming = () => {
+  return <div className={style.live}>경기 중입니다.</div>
+}
+
+const NonLiveStreaming = ({ score }) => {
+  return score ?
+    <div className={style.live}>경기 종료</div> :
+    <div className={style.live}>아직 경기 전입니다</div>
+}
 
 const TodayGame = ({ todayGame, winner }) => {
   const { date, isLive, away, home } = todayGame;
 
   return (
     <div>
-      <div className="prev-game">
+      <div className={style.result}>
         {winner === 'KT' ? <div>YES</div> : <div>NO</div>}
       </div>
-      <div className="current-game">
-        {isLive ? <div>경기 중입니다.</div> : <div>경기 종료</div>}
-        <div className="score">
-          <div>{date}</div>
-          <span>{away.team}: {away.score}</span>
-          <span> vs</span>
-          <span> {home.team}: {home.score}</span>
-        </div>
+      <div className={style.current}>
+        {isLive ?
+          <>
+            <LiveStreaming />
+            <ScoreBoard date={date} away={away} home={home} />
+          </> :
+          <>
+            <NonLiveStreaming />
+            <ScoreBoard date={date} away={away} home={home} />
+          </>
+        }
       </div>
-      <style jsx>{`
-        .prev-game {
-          font-weight: bold;
-          font-size: 5rem;
-        }
-        .current-game {
-          margin-top: 1rem;
-          font-size: 1.3rem;
-        }
-        .score {
-          margin-top: 0.5rem;
-        }
-      `}
-      </style>
     </div>
   )
 }
