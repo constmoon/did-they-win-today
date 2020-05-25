fd69e4525b37ed84941add6f956cf4988dac9224
constmoon <jiwoo.link@gmail.com>
2020-05-26 05:17:19 +0900

    feat: 직전일이 월요일일 경우 표시 변경

--- utils/gameData.js
+++ utils/gameData.js
@@ -17,7 +17,8 @@ const useTodayGame = () => {
       const gameList = await fetchGameList();
       const date = new Date;
       const index = date.getDate() - 1;
-      if (gameList[index].away.score === null) {
+      // TODO: 아직 경기가 없고 직전일이 월요일(무게임)일 경우 오늘 예정된 게임 표시...를 좀 더 다른 방법으로
+      if (!gameList[index].away.score && !gameList[index - 1].date.includes('월')) {
         setTodayGame(gameList[index - 1]);
       }
       else {
