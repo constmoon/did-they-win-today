a0e14b8446e82ec759173eef417ed74d26d7c2e6
constmoon <jiwoo.link@gmail.com>
2020-08-30 21:39:25 +0900

    feat: 팀 목록 받아오는 api 생성

---
+++ pages/api/teamList.js
@@ -0,0 +1,57 @@
+export default (req, res) => {
+  const teamList = [
+    {
+      id: 1,
+      code: 'OB',
+      name: '두산'
+    },
+    {
+      id: 2,
+      code: 'WO',
+      name: '키움'
+    },
+    {
+      id: 3,
+      code: 'SK',
+      name: 'SK'
+    },
+    {
+      id: 4,
+      code: 'LG',
+      name: 'LG'
+    },
+    {
+      id: 5,
+      code: 'NC',
+      name: 'NC'
+    },
+    {
+      id: 6,
+      code: 'KT',
+      name: 'KT'
+    },
+    {
+      id: 7,
+      code: 'HT',
+      name: 'KIA'
+    },
+    {
+      id: 8,
+      code: 'SS',
+      name: '삼성'
+    },
+    {
+      id: 9,
+      code: 'HH',
+      name: '한화'
+    },
+    {
+      id: 10,
+      code: 'LT',
+      name: '롯데'
+    }]
+
+  res.status(200).json({
+    teamList
+  });
+}
